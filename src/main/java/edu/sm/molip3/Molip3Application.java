7cf34ed6f8d97d515eceb3b9928f1d3d38fea1a2
ingcheol <sic0804@naver.com>
2025-07-21 15:48:01 +0900

    0721

---
+++ src/main/java/edu/sm/molip3/Molip3Application.java
@@ -0,0 +1,13 @@
+package edu.sm.molip3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Molip3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Molip3Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/sm/molip3/Molip3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sm.molip3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Molip3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
